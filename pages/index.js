77f7914a71d3e7c05bd5ddd10c7db5232683fa76
Amo Moloko <u14284783@tuks.co.za>
2019-08-27 11:49:53 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,14 @@ export default () => (
     <Link href="/about">
       <a>About</a>
     </Link>
+    <div className="flex flex-row">
+      <p className="bg p-4">Hello</p>
+
+      <p className="bg p-4">Hello</p>
+
+      <p className="bg-blue-400 p-4">Hello</p>
+    </div>
+    <p className="bg-blue-400 p-4">Hello</p>
     <HeadingOne className="bg-pinkDarkest" text="hello" />
   </div>
 );
